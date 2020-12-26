81b47181b31112bc0f345df5f6c6bb29a6a1743f
Aletia <a.holloway2789@gmail.com>
2020-12-26 00:13:19 -0500

    Unit 1 Lab 1 Deliverable Final Copy

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Deliverables_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Create a Password Below. Password Must Contain: \n \tAt least one lowercase letter." +
+                "\n\tAt least one uppercase letter. \n\tA minimum of seven characters. " +
+                "\n\tA maximum of twelve characters.\n\tAn exclaimation point");
+            string password = Console.ReadLine();
+          
+            bool hasUpper = false;
+            bool hasLower = false;
+            for (int i = 0; i<password.Length; i++) 
+            { 
+                if (char.IsUpper(password[i])) { hasUpper = true; }
+                if (char.IsLower(password[i])) { hasLower = true; }
+            } 
+
+
+            if (password.Length >= 7 && password.Length <= 12) 
+            {
+                if (password.Contains("!")) 
+                {
+                    if (hasLower && hasUpper) 
+                    { 
+                        Console.WriteLine("Password valid and accepted"); 
+                    }
+                    else { Console.WriteLine("Error"); }
+
+                }
+                else { Console.WriteLine("Error"); }
+
+            }
+            else { Console.WriteLine("Error"); }
+        }
+
+
+
+    }
+}
